728fd35e27b46dd9d8188bedaf4add4078be2a3c
Judi Perez <j_vperez29@bellsouth.net>
2018-02-09 20:14:12 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.tvHbjQQOnKAPvabcOa2r5mdg7secret,
+  consumer_secret: process.env.DHYcdGAkljCvQCeyhyXCkOqUVG7PTE93xxbw97YdntUIPW34,
+  access_token_key: process.env.70090401-yXpJ0PuaKg2wVmS5JwiAC3FUdTsj44rbytiZl8IxQ,
+  access_token_secret: process.env.bYuQnShRZy2pZeDZHyH3F38JEZRBejgtRMd7K7Amy9BfG
+
+};
+
+exports.spotify = {
+  id: process.env.6786b0e78fcc42a1a80980c69ca84bcf,
+  secret: process.env.65b3cc744bb2465d8a2527d7930750e0
+};
+
